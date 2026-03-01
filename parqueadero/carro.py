46c58c47201d9b6dfe91a6082f62d3f3b15ca8dc
AndresDavidOsorio <andrumec2005@gmail.com>
2026-03-01 16:59:53 -0500

    Delete parqueadero directory

--- parqueadero/carro.py
+++
@@ -1,8 +0,0 @@
-class Carro:
-    def __init__(self, placa, tipo_carro, color):
-        self.placa = placa
-        self.tipo_carro = tipo_carro
-        self.color = color
-
-    def mostrar(self):
-        return f"{self.placa} - {self.tipo_carro} - {self.color}"
--- parqueadero/usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, cedula, nombre, tipo_usuario):
-        self.cedula = cedula
-        self.nombre = nombre
-        self.tipo_usuario = tipo_usuario
-
-    def mostrar(self):
-        return f"{self.cedula} - {self.nombre} - {self.tipo_usuario}"
-    
